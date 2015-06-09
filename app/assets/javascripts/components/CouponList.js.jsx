c321c4a27a82711301d8e90dcf7dc8d4eaf5488a
Frank Kalbermatten <frankkalbermatten@gmail.com>
2015-06-09 00:26:54 -0300

    adding new react files

--- app/assets/javascripts/components/CouponList.js.jsx
+++ app/assets/javascripts/components/CouponList.js.jsx
@@ -41,8 +41,10 @@ var CouponList = React.createClass({
             this.state.items.map (function (item, index) {
                 return (
                     <li className="coupon-list" id={'coupons_' + item.id}  key={index}>
-                        <span className="coupon-list--item">{'Coupon ' + item.id}</span>
-                        <span className="coupon-list--title">{item.title}</span>
+                        <span className="coupon-list--container">
+                            <span className="coupon-list--item">{'Coupon ' + item.id}</span>
+                            <span className="coupon-list--title">{item.title}</span>
+                        </span>
                         <CouponActions className="couponActions" couponId={item.id} />
                     </li>
                 );
