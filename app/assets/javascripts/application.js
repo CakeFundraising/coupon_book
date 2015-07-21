be0744523f2d18707aba8e3eb199c9b4376db141
Emiliano Coppo <bismark64@gmail.com>
2015-07-21 16:07:58 -0300

    Purchase modal fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,9 +26,8 @@
 //= require cloudinary/jquery.fileupload
 //= require ./jquery.cloudinary
 
-//= require bootstrap
-
 //= require jquery-ui
+//= require bootstrap
 
 //= require zeroclipboard
 //= require bootstrap-datepicker/core
