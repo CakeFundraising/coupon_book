9264eecae057e06836f926e744e9db772ef6bfce
Emiliano Coppo <bismark64@gmail.com>
2015-08-25 15:28:34 -0300

    Fix Account picture cloudinary call

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -8,7 +8,7 @@ class MerchantsController < InheritedResources::Base
       :first_name, :last_name, :email, :phone, :tax_exempt,
       location_attributes: [:address, :city, :zip_code, :state_code, :country_code],
       avatar_picture_attributes: [
-        :id, :uri, :caption, :avatar_crop_x, :avatar_crop_y, :avatar_crop_w, :avatar_crop_h
+        :id, :uri, :caption, :avatar_crop_x, :avatar_crop_y, :avatar_crop_w, :avatar_crop_h, :bypass_cloudinary_validation
       ]
     ])
   end
