924e8b720872a972b6164fcf2818de97b20d8352
Emiliano Coppo <bismark64@gmail.com>
2015-09-22 18:12:42 -0300

    Merchant fixes

--- app/controllers/concerns/account_controller.rb
+++ app/controllers/concerns/account_controller.rb
@@ -42,6 +42,6 @@ module AccountController
   end
 
   def check_if_account_created
-    redirect_to root_path, alert:"Please sign out and create a new account if you want to create a new account." if current_user.nil?
+    redirect_to root_path, alert:"Please sign up to create a new account." if current_user.nil?
   end
 end
