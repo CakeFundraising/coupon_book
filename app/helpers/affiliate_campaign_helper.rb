b1aa111db34d44a3e46d0af9c8f559fa0ad98a4d
Emiliano Coppo <bismark64@gmail.com>
2015-10-06 12:45:04 -0300

    Affiliate fixes

--- app/helpers/affiliate_campaign_helper.rb
+++ app/helpers/affiliate_campaign_helper.rb
@@ -1,7 +1,7 @@
 module AffiliateCampaignHelper
   def affiliate_campaign_edit_button(campaign)
     if can? :edit, campaign and not campaign.coupon_book.past?
-      link_to "Edit Campaign", edit_affiliate_campaign_path(campaign), class:'btn btn-transparent-dark'
+      link_to "Edit Campaign", join_affiliate_campaign_path(campaign), class:'btn btn-transparent-dark'
     end
   end
 
