b8b204117ce39c526157a7ae309ae9b565293527
Emiliano Coppo <bismark64@gmail.com>
2015-11-11 17:07:56 -0300

    Fix AvatarPicture decorator

--- app/decorators/avatar_picture_decorator.rb
+++ app/decorators/avatar_picture_decorator.rb
@@ -4,7 +4,7 @@ class AvatarPictureDecorator < ApplicationDecorator
   def uri(options={})
     if object.uri.present?
       classes = options.key?(:no_classes) ? '' : 'img-thumbnail img-responsive img-loaded'
-      options = {crop: :crop, width: object.avatar_crop_w, height: object.avatar_crop_h, x: object.avatar_crop_x, y: object.avatar_crop_y, transformation: :coupon_grid_sp, quality: :jpegmini, sign_url: true, class: classes}.merge options
+      options = {crop: :crop, width: object.avatar_crop_w, height: object.avatar_crop_h, x: object.avatar_crop_x, y: object.avatar_crop_y, quality: :jpegmini, sign_url: true, class: classes}.merge options
       h.cl_image_tag(object.uri, options)
     else
       h.image_tag 'placeholder_avatar.png', class: 'img-thumbnail img-responsive img-default-avatar'
