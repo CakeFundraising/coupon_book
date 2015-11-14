52e7290bc79f4b5412932f91c5a3b0b8c9d9cbd7
Emiliano Coppo <bismark64@gmail.com>
2015-11-14 19:53:42 -0300

    Remove step 5 from Commission rates

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -5,6 +5,6 @@ module CommunitiesHelper
 
   def media_commission_rate(affiliate_rate)
     up_to = 100 - affiliate_rate
-    (0..up_to).step(5).to_a.map{|v| ["#{v}%", v]}
+    (0..up_to).to_a.map{|v| ["#{v}%", v]}
   end
 end
